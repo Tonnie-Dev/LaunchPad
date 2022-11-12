ff7156ac5fca14d74a5c59fe8ba9666e2bad6190
Tonnie <vontonnie@gmail.com>
2022-11-12 11:41:38 +0300

    Upgrade Kotlin version from 1.7 to 1.9

--- app/src/main/java/com/uxstate/launchpad/MainActivity.kt
+++ app/src/main/java/com/uxstate/launchpad/MainActivity.kt
@@ -19,8 +19,8 @@ class MainActivity : ComponentActivity() {
             LaunchPadTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
                 }
