1a03bfc166d0591f43ef0060901a3be9fa826549
Tonnie <vontonnie@gmail.com>
2022-11-13 13:55:08 +0300

    Add RemoteKeysDao interface query functions

--- app/src/main/java/com/uxstate/launchpad/data/local/dao/RemoteKeysDao.kt
+++ app/src/main/java/com/uxstate/launchpad/data/local/dao/RemoteKeysDao.kt
@@ -1,4 +1,20 @@
 package com.uxstate.launchpad.data.local.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.uxstate.launchpad.data.local.entities.RemoteKeysEntity
+
+
+@Dao
 interface RemoteKeysDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRemoteKeys(keys: List<RemoteKeysEntity>)
+
+    @Query("SELECT * FROM remote_keys_table WHERE id=:id")
+    suspend fun getRemoteKeyById(id: Int): RemoteKeysEntity
+
+    @Query("DELETE FROM remote_keys_table")
+    suspend fun clearRemoteKeys()
 }
