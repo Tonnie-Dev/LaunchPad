8aa6aff953389d685a16c9c998fe96dbc0e4536e
Tonnie <vontonnie@gmail.com>
2022-11-13 14:02:44 +0300

    Add LaunchDao inteface functions

--- app/src/main/java/com/uxstate/launchpad/data/local/dao/LaunchDao.kt
+++ app/src/main/java/com/uxstate/launchpad/data/local/dao/LaunchDao.kt
@@ -1,6 +1,23 @@
 package com.uxstate.launchpad.data.local.dao
 
+import androidx.paging.PagingData
+import androidx.paging.PagingSource
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.uxstate.launchpad.data.local.entities.LaunchEntity
+import com.uxstate.launchpad.domain.model.Launch
 
 @Dao
-interface LaunchDao
+interface LaunchDao{
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertLaunches(launches: List<LaunchEntity>)
+
+    @Query("SELECT * FROM launch_table")
+    suspend fun getLaunches():PagingSource<String,Launch>
+
+    @Query("DELETE FROM launch_table")
+    suspend fun clearLaunches()
+}
