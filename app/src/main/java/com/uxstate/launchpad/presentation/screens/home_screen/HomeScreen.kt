163fa483bcb8fe8e99be792acf8c3e237211ab2a
Tonnie <vontonnie@gmail.com>
2022-11-14 21:12:12 +0300

    Create HomeScreen file

---
+++ app/src/main/java/com/uxstate/launchpad/presentation/screens/home_screen/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.uxstate.launchpad.presentation.screens.home_screen
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun HomeScreen() {
+
+    
+
+}
---
+++ app/src/main/java/com/uxstate/launchpad/presentation/screens/home_screen/HomeViewModel.kt
@@ -0,0 +1,14 @@
+package com.uxstate.launchpad.presentation.screens.home_screen
+
+import androidx.lifecycle.ViewModel
+import com.uxstate.launchpad.domain.use_cases.UseCaseWrapper
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val useCaseWrapper: UseCaseWrapper
+) : ViewModel() {
+
+    val previousLaunches = useCaseWrapper.getPreviousLaunchesUseCase
+}
