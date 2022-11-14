baba4ca39c69cc6db5facc920c0c41a3cfb51ae1
Tonnie <vontonnie@gmail.com>
2022-11-14 20:41:38 +0300

    Add provideLaunchRepository abstract di function for Repository module

--- app/src/main/java/com/uxstate/launchpad/di/RepositoryModule.kt
+++ app/src/main/java/com/uxstate/launchpad/di/RepositoryModule.kt
@@ -1,10 +1,19 @@
 package com.uxstate.launchpad.di
 
+import com.uxstate.launchpad.data.repository.LaunchRepositoryImpl
+import com.uxstate.launchpad.domain.repository.LaunchRepository
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+   abstract fun provideLaunchRepository (repositoryImpl: LaunchRepositoryImpl):LaunchRepository
+   
 }
