14edc448af83ff4b8392be506c3cca52eb434cf1
Tonnie <vontonnie@gmail.com>
2022-11-14 21:13:39 +0300

    Delete unused code from MainActivity

--- app/src/main/java/com/uxstate/launchpad/presentation/MainActivity.kt
+++ app/src/main/java/com/uxstate/launchpad/presentation/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.uxstate.launchpad.presentation.ui.theme.LaunchPadTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +24,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LaunchPadTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/uxstate/launchpad/presentation/screens/home_screen/HomeScreen.kt
+++ app/src/main/java/com/uxstate/launchpad/presentation/screens/home_screen/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.uxstate.launchpad.presentation.screens.home_screen
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import com.ramcosta.composedestinations.annotation.Destination
 
@@ -7,6 +8,6 @@ import com.ramcosta.composedestinations.annotation.Destination
 @Composable
 fun HomeScreen() {
 
-    
+    Text(text = "The Richest Man in Babylon")
 
 }
