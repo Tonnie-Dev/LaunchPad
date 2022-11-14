939fc67e2ed5218a505adeb1021f119883d5908a
Tonnie <vontonnie@gmail.com>
2022-11-14 19:56:48 +0300

    Overried getPreviousLaunches() repository impl function

--- app/src/main/java/com/uxstate/launchpad/data/repository/LaunchRepository.kt
+++ app/src/main/java/com/uxstate/launchpad/data/repository/LaunchRepository.kt
@@ -1,8 +1,19 @@
 package com.uxstate.launchpad.data.repository
 
+import androidx.paging.PagingData
+import com.uxstate.launchpad.domain.model.Launch
 import com.uxstate.launchpad.domain.paging_source.RemoteDataSource
 import com.uxstate.launchpad.domain.repository.LaunchRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class LaunchRepositoryImpl @Inject constructor(private val dataSource: RemoteDataSource):LaunchRepository {
+class LaunchRepositoryImpl @Inject constructor(
+    private val dataSource:
+    RemoteDataSource
+) : LaunchRepository {
+
+    override fun getPreviousLaunches(): Flow<PagingData<Launch>> {
+
+        return dataSource.getLaunches()
+    }
 }
