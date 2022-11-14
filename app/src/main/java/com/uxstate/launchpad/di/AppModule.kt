fb3406541b947fc10cde542f21f6d577a1ee194c
Tonnie <vontonnie@gmail.com>
2022-11-14 20:25:43 +0300

    Add provideHttpLogginInterceptor di function

--- app/src/main/java/com/uxstate/launchpad/di/AppModule.kt
+++ app/src/main/java/com/uxstate/launchpad/di/AppModule.kt
@@ -6,9 +6,13 @@ import androidx.room.RoomDatabase
 import com.uxstate.launchpad.data.local.LaunchDatabase
 import com.uxstate.launchpad.util.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -16,10 +20,32 @@ import dagger.hilt.components.SingletonComponent
 
 object AppModule {
 
+    @Provides
+    @Singleton
+
     fun provideDatabase(@ApplicationContext context: Context): RoomDatabase {
 
         return Room.databaseBuilder(context, LaunchDatabase::class.java, Constants.DATABASE_NAME)
                 .build()
     }
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor():HttpLoggingInterceptor {
+
+        return HttpLoggingInterceptor().apply {
+
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+
+    @Provides
+    @Singleton
+
+    fun provideOkHttpClient ():OkHttpClient {
+
+        return OkHttpClient().apply { cont }
+    }
+
 
 }
--- app/src/main/java/com/uxstate/launchpad/util/Constants.kt
+++ app/src/main/java/com/uxstate/launchpad/util/Constants.kt
@@ -5,4 +5,6 @@ object Constants {
     const val ITEMS_PER_PAGE_LIMIT = 10
     const val OFFSET_STARTING_INDEX = 0
     const val DATABASE_NAME = "LaunchDatabase"
+    const val READ_TIMEOUT = 15L
+    const val CONNECT_TIMEOUT = 15L
 }
