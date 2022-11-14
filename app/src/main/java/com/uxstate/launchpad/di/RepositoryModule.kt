b8d97e7f84e2f20cd00d56f07b664150d612060c
Tonnie <vontonnie@gmail.com>
2022-11-14 20:44:00 +0300

    Add bindRemoteDataSource() abstract di function

--- app/src/main/java/com/uxstate/launchpad/di/RepositoryModule.kt
+++ app/src/main/java/com/uxstate/launchpad/di/RepositoryModule.kt
@@ -1,6 +1,8 @@
 package com.uxstate.launchpad.di
 
+import com.uxstate.launchpad.data.remote.paging_source.RemoteDataSourceImpl
 import com.uxstate.launchpad.data.repository.LaunchRepositoryImpl
+import com.uxstate.launchpad.domain.paging_source.RemoteDataSource
 import com.uxstate.launchpad.domain.repository.LaunchRepository
 import dagger.Binds
 import dagger.Module
@@ -14,6 +16,14 @@ abstract class RepositoryModule {
 
     @Binds
     @Singleton
-   abstract fun provideLaunchRepository (repositoryImpl: LaunchRepositoryImpl):LaunchRepository
-   
+    abstract fun bindLaunchRepository(
+        repositoryImpl: LaunchRepositoryImpl
+    ): LaunchRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindRemoteDataSource(
+        remoteDataSourceImpl: RemoteDataSourceImpl
+    ): RemoteDataSource
+
 }
