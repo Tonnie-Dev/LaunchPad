d11e29e4de99d45b7a9246df908d9ce8dc47944b
Tonnie <vontonnie@gmail.com>
2022-11-14 19:53:25 +0300

    Add LaunchRepository interface definition

---
+++ app/src/main/java/com/uxstate/launchpad/domain/repository/LaunchRepository.kt
@@ -0,0 +1,10 @@
+package com.uxstate.launchpad.domain.repository
+
+import androidx.paging.PagingData
+import com.uxstate.launchpad.domain.model.Launch
+import kotlinx.coroutines.flow.Flow
+
+interface LaunchRepository {
+
+    fun getPreviousLaunches():Flow<PagingData<Launch>>
+}
