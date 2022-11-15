8eb0dd5455e2fc23a036b6b158bcdd7334d01a67
Tonnie <vontonnie@gmail.com>
2022-11-15 07:57:06 +0300

    Create placeholder_image drawable vector asset

--- app/src/main/java/com/uxstate/launchpad/presentation/screens/home_screen/components/LaunchImage.kt
+++ app/src/main/java/com/uxstate/launchpad/presentation/screens/home_screen/components/LaunchImage.kt
@@ -4,9 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
+import com.uxstate.launchpad.domain.model.Launch
+import com.uxstate.launchpad.util.LocalSpacing
 
 @Composable
-fun LaunchImage() {
+fun LaunchImage(launch:Launch) {
+    val spacing = LocalSpacing.current
     val context = LocalContext.current
-    val painter = rememberAsyncImagePainter(model = ImageRequest.Builder())
+    val painter = rememberAsyncImagePainter(model = ImageRequest.Builder(context = context).data(launch.imageUrl).placeholder())
 }
