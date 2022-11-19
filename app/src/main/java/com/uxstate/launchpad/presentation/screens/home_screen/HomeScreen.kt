306d096662ee2a3705afd21fe11e54a7a0820bc4
Tonnie <vontonnie@gmail.com>
2022-11-19 17:34:06 +0300

    Create LaunchList composable

--- app/src/main/java/com/uxstate/launchpad/presentation/screens/home_screen/HomeScreen.kt
+++ app/src/main/java/com/uxstate/launchpad/presentation/screens/home_screen/HomeScreen.kt
@@ -3,15 +3,13 @@ package com.uxstate.launchpad.presentation.screens.home_screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
-import androidx.paging.compose.items
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
-import com.uxstate.launchpad.presentation.screens.home_screen.components.LaunchImage
+import com.uxstate.launchpad.presentation.screens.home_screen.components.LaunchList
 import com.uxstate.launchpad.util.LocalSpacing
 
 @Destination()
@@ -29,15 +27,6 @@ fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
             .padding()
     ) {
 
-        LazyColumn {
-
-            items(items = upcomingLaunches) { launch ->
-
-                launch?.let {
-
-                    LaunchImage(launch = launch)
-                }
-            }
-        }
+        LaunchList(data = previousLaunches)
     }
 }
---
+++ app/src/main/java/com/uxstate/launchpad/presentation/screens/home_screen/components/LaunchList.kt
@@ -0,0 +1,23 @@
+package com.uxstate.launchpad.presentation.screens.home_screen.components
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.items
+import com.uxstate.launchpad.domain.model.Launch
+
+@Composable
+fun LaunchList(data: LazyPagingItems<Launch>, modifier: Modifier = Modifier) {
+
+    LazyColumn(modifier = modifier, content = {
+
+        items(data) { launch ->
+
+            launch?.let {
+
+                LaunchImage(launch = launch)
+            }
+        }
+    })
+}
