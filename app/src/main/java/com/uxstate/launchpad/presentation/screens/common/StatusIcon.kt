9fb4c3f252998dd0d6a131e1d09e63d288a4f65e
Tonnie <vontonnie@gmail.com>
2022-11-27 07:52:56 +0300

    Generate iconTint colors for status icon

---
+++ app/src/main/java/com/uxstate/launchpad/presentation/screens/common/StatusIcon.kt
@@ -0,0 +1,23 @@
+package com.uxstate.launchpad.presentation.screens.common
+
+import androidx.compose.runtime.Composable
+import com.uxstate.launchpad.domain.model.Launch
+import com.uxstate.launchpad.presentation.ui.theme.DahliaYellow
+import com.uxstate.launchpad.presentation.ui.theme.LuminousRed
+import com.uxstate.launchpad.presentation.ui.theme.MintGreen
+import com.uxstate.launchpad.presentation.ui.theme.OysterWhite
+
+@Composable
+fun StatusIcon(launch: Launch) {
+
+    val iconTint = when (launch.status) {
+
+
+        "Success" -> MintGreen
+        "Go" -> MintGreen
+        "TBC" -> DahliaYellow
+        "Failure" -> LuminousRed
+        else -> OysterWhite
+    }
+
+}
--- app/src/main/java/com/uxstate/launchpad/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/uxstate/launchpad/presentation/ui/theme/Color.kt
@@ -2,7 +2,14 @@ package com.uxstate.launchpad.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+//Defaults
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+//Status Icon Tint Colors
+val LuminousRed = Color(0xFFFE0000)
+val DahliaYellow = Color(0xFFF3A505)
+val OysterWhite= Color(0xFFEAE6CA)
+val MintGreen= Color(0xFF06F773)
