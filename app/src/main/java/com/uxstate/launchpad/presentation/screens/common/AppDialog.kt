f5ed2d049a60bdd52682e420799c63eb7735c32d
Tonnie <vontonnie@gmail.com>
2022-12-02 08:41:47 +0300

    Delete unused code from components

--- app/src/main/java/com/uxstate/launchpad/presentation/screens/common/AppDialog.kt
+++ app/src/main/java/com/uxstate/launchpad/presentation/screens/common/AppDialog.kt
@@ -1,7 +1,11 @@
 package com.uxstate.launchpad.presentation.screens.common
 
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.uxstate.launchpad.util.LocalSpacing
 
 @Composable
@@ -9,9 +13,29 @@ fun AppDialog(
     modifier: Modifier = Modifier,
     dialogState: Boolean = false,
     onDialogPositiveButton: (() -> Unit)? = null,
-    onDialogStateChange:((Boolean) -> Unit)? = null,
-    onDismiss: (()-> Unit)? = null
+    onDialogStateChange: ((Boolean) -> Unit)? = null,
+    onDismiss: (() -> Unit)? = null
 ) {
 
     val spacing = LocalSpacing.current
+    val dialogShape = RoundedCornerShape(spacing.spaceMedium)
+
+    if (dialogState) {
+
+        AlertDialog(
+            onDismissRequest = {
+                onDialogStateChange?.invoke(false)
+                onDismiss?.invoke()
+            },
+            title = { Text(text = "Error") },
+            text = { Text(text = "Launch not found!") }, buttons = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AppDialogPreview() {
+
+    AppDialog()
 }
