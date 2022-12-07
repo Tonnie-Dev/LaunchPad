d8cc7a6674bc445c0b07715162f6ed3478867e7d
Tonnie <vontonnie@gmail.com>
2022-12-07 17:34:11 +0300

    Add AnimatedShimmerItem composable function

--- app/src/main/java/com/uxstate/launchpad/presentation/screens/common/ShimmerEffect.kt
+++ app/src/main/java/com/uxstate/launchpad/presentation/screens/common/ShimmerEffect.kt
@@ -1,8 +1,10 @@
 package com.uxstate.launchpad.presentation.screens.common
 
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -13,47 +15,67 @@ import com.uxstate.launchpad.presentation.ui.theme.Black400
 import com.uxstate.launchpad.presentation.ui.theme.ShimmerDarkGray
 import com.uxstate.launchpad.util.LocalSpacing
 
+
+@Composable
+fun AnimatedShimmerItem() {
+
+    val transition = rememberInfiniteTransition()
+
+    val alphaValue by transition.animateFloat(
+            initialValue = 1f,
+            targetValue = 0f,
+            animationSpec = infiniteRepeatable(
+                    animation = tween(
+                            durationMillis = 500,
+                            easing = FastOutSlowInEasing
+                    ), repeatMode = RepeatMode.Reverse
+            )
+    )
+
+    ShimmerPlaceholderItem(alpha = alphaValue)
+}
+
 @Composable
 fun ShimmerPlaceholderItem(alpha: Float, modifier: Modifier = Modifier) {
     val spacing = LocalSpacing.current
     Surface(
-        modifier = modifier,
-        color = Black400
+            modifier = modifier,
+            color = Black400
     ) {
         Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(19f / 20f)
-                .padding(spacing.spaceSmall),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Bottom
+                modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(19f / 20f)
+                        .padding(spacing.spaceSmall),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Bottom
         ) {
             ShimmerBar(
-                alpha = alpha,
-                height = spacing.spaceLarge,
-                modifier = Modifier.fillMaxWidth(.7f)
+                    alpha = alpha,
+                    height = spacing.spaceLarge,
+                    modifier = Modifier.fillMaxWidth(.7f)
             )
 
             Spacer(modifier = Modifier.height(spacing.spaceSmall))
             ShimmerBar(
-                alpha = alpha,
-                height = spacing.spaceMedium,
-                modifier = Modifier.fillMaxWidth(.3f)
+                    alpha = alpha,
+                    height = spacing.spaceMedium,
+                    modifier = Modifier.fillMaxWidth(.3f)
             )
 
             Spacer(modifier = Modifier.height(spacing.spaceSmall))
             ShimmerBar(
-                alpha = alpha,
-                height = spacing.spaceMedium,
-                modifier = Modifier.fillMaxWidth(.4f)
+                    alpha = alpha,
+                    height = spacing.spaceMedium,
+                    modifier = Modifier.fillMaxWidth(.4f)
             )
             Spacer(modifier = Modifier.height(spacing.spaceSmall))
 
             Row {
                 repeat(5) {
                     ShimmerBar(
-                        alpha = alpha,
-                        modifier = Modifier.size(spacing.spaceLarge)
+                            alpha = alpha,
+                            modifier = Modifier.size(spacing.spaceLarge)
                     )
                     Spacer(modifier = Modifier.width(spacing.spaceExtraSmall))
                 }
@@ -66,11 +88,11 @@ fun ShimmerPlaceholderItem(alpha: Float, modifier: Modifier = Modifier) {
 fun ShimmerBar(alpha: Float, modifier: Modifier = Modifier, height: Dp = 0.dp) {
     val spacing = LocalSpacing.current
     Surface(
-        modifier = modifier
-            .height(height)
-            .alpha(alpha),
-        color = ShimmerDarkGray,
-        elevation = spacing.spaceExtraSmall
+            modifier = modifier
+                    .height(height)
+                    .alpha(alpha),
+            color = ShimmerDarkGray,
+            elevation = spacing.spaceExtraSmall
     ) {}
 }
 
