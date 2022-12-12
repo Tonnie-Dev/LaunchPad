cedd17a224b29b33f8911fbcfa912db6fe495a0d
Tonnie <vontonnie@gmail.com>
2022-12-12 12:11:37 +0300

    Replace single quotes with double quotes for build.gradle(app)

--- app/src/main/java/com/uxstate/launchpad/di/AppModule.kt
+++ app/src/main/java/com/uxstate/launchpad/di/AppModule.kt
@@ -97,3 +97,23 @@ object AppModule {
         )
     }
 }
+
+
+/*
+   */
+/* ext {
+        compose_ui_version = '1.3.1'
+        paging_compose = "1.0.0-alpha16"
+        swipe_refresh = "0.24.7-alpha"
+        shimmer = "0.5.0"
+        accompanist_version = '0.28.0'
+    }*//*
+
+
+
+}// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id 'com.android.application' version '7.3.1' apply false
+    id 'com.android.library' version '7.3.1' apply false
+    id 'org.jetbrains.kotlin.android' version '1.7.20' apply false
+}*/
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,17 @@
 buildscript {
-    ext {
-        compose_ui_version = '1.3.1'
-        paging_compose = "1.0.0-alpha16"
-        swipe_refresh = "0.24.7-alpha"
-        shimmer = "0.5.0"
-        accompanist_version = '0.28.0'
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
     }
 
-    dependencies {
-        classpath "com.google.dagger:hilt-android-gradle-plugin:2.44"
-    }
-}// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id 'com.android.application' version '7.3.1' apply false
-    id 'com.android.library' version '7.3.1' apply false
-    id 'org.jetbrains.kotlin.android' version '1.7.20' apply false
 }
+
+plugins{
+
+    id ("com.android.application") version "7.3.1" apply false
+    id("com.android.library") version "7.3.1" apply false
+    id ("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id ("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+}
+
+
