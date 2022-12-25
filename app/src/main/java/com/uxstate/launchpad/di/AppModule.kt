ebe4a24e932e7f92c41c0bed6c762ebd617e3bf5
Tonnie <vontonnie@gmail.com>
2022-12-25 15:35:03 +0300

    Remove unused code from AppModule file

--- app/src/main/java/com/uxstate/launchpad/di/AppModule.kt
+++ app/src/main/java/com/uxstate/launchpad/di/AppModule.kt
@@ -28,7 +28,6 @@ import retrofit2.create
 
 @Module
 @InstallIn(SingletonComponent::class)
-@RequiresApi(Build.VERSION_CODES.O)
 object AppModule {
 
     @Provides
@@ -99,21 +98,4 @@ object AppModule {
     }
 }
 
-/*
-   */
-/* ext {
-        compose_ui_version = '1.3.1'
-        paging_compose = "1.0.0-alpha16"
-        swipe_refresh = "0.24.7-alpha"
-        shimmer = "0.5.0"
-        accompanist_version = '0.28.0'
-    }*//*
 
-
-
-}// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id 'com.android.application' version '7.3.1' apply false
-    id 'com.android.library' version '7.3.1' apply false
-    id 'org.jetbrains.kotlin.android' version '1.7.20' apply false
-}*/
