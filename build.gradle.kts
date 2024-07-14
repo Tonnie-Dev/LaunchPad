11ba5247a4c74981aab72e44ad9a2f0a6c5c459d
Tonnie <vontonnie@gmail.com>
2024-07-14 16:44:40 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,11 @@
-buildscript {
-
-    repositories{
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-// features/add-version-catalogs
-        classpath(Android.tools.build.gradlePlugin)
-        classpath (libs.kotlin.gradle.plugin)
-        classpath(Google.dagger.hilt.android.gradlePlugin)
-        classpath(libs.spotless.plugin.gradle)
-    }
-
+plugins {
+    id("com.android.application") version "8.1.4" apply false
+    id ("com.android.library") version "8.1.4" apply false
+    id ("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id ("com.google.dagger.hilt.android")  version "2.48" apply false
+    id ("com.google.devtools.ksp") version  "1.9.10-1.0.13" apply false
+    id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.10" apply false
+    id("com.diffplug.spotless") version "5.0.0" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1" apply false
 }
-
 
