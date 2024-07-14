a5fc7cbe3d1dbcb31f7a0a51852764d35fb29f4d
Tonnie <vontonnie@gmail.com>
2024-07-14 16:44:47 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,8 @@ dependencyResolutionManagement {
 }
 plugins {
     // See https://jmfayard.github.io/refreshVersions
-    id("de.fayard.refreshVersions") version "0.51.0"
+    id("de.fayard.refreshVersions") version "0.60.5"
+
 }
 
 rootProject.name = "LaunchPad"
