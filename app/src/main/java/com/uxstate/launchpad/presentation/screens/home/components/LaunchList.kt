def594ab7878a43004ccd5218746482f2d30ef3d
Tonnie <vontonnie@gmail.com>
2024-07-21 16:08:14 +0300

    Update LaunchList.kt

--- app/src/main/java/com/uxstate/launchpad/presentation/screens/home/components/LaunchList.kt
+++ app/src/main/java/com/uxstate/launchpad/presentation/screens/home/components/LaunchList.kt
@@ -2,11 +2,14 @@ package com.uxstate.launchpad.presentation.screens.home.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
-import androidx.paging.compose.items
+import androidx.paging.compose.itemContentType
+import androidx.paging.compose.itemKey
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.uxstate.launchpad.domain.model.Launch
 import com.uxstate.launchpad.presentation.screens.common.EmptyScreen
@@ -19,25 +22,31 @@ fun LaunchList(
     showCountDown: Boolean,
     navigator: DestinationsNavigator,
     modifier: Modifier = Modifier,
-) {
+
+    ) {
 
     val result = handlePagingResult(launches = data)
 
     if (result) {
         LazyColumn(modifier = modifier, content = {
 
-            items(data) { launch ->
+            items(
+                    count = data.itemCount,
+                    key = data.itemKey { it.id },
+                    contentType = data.itemContentType{"Launch"}
+            ) { i ->
 
+                val launch = data[i]
                 launch?.let {
 
                     LaunchImage(
-                        launch = it,
-                        showCountDown = showCountDown,
-                        modifier = Modifier.clickable {
-                            navigator.navigate(
-                                DetailsScreenDestination(it)
-                            )
-                        }
+                            launch = it,
+                            showCountDown = showCountDown,
+                            modifier = Modifier.clickable {
+                                navigator.navigate(
+                                        DetailsScreenDestination(it)
+                                )
+                            }
 
                     )
                 }
@@ -45,6 +54,7 @@ fun LaunchList(
         })
     }
 }
+
 // starts with small letter because this function returns a value
 @Composable
 fun handlePagingResult(launches: LazyPagingItems<Launch>): Boolean {
@@ -57,9 +67,11 @@ fun handlePagingResult(launches: LazyPagingItems<Launch>): Boolean {
             loadState.refresh is LoadState.Error -> {
                 loadState.refresh as LoadState.Error
             }
+
             loadState.prepend is LoadState.Error -> {
                 loadState.prepend as LoadState.Error
             }
+
             loadState.append is LoadState.Error -> {
                 loadState.append as LoadState.Error
             }
