744c4edf863daa15815ed3b53e5b216511a186f3
Tonnie <vontonnie@gmail.com>
2024-08-04 11:06:23 +0300

    Update Theme.kt

--- app/src/main/java/com/uxstate/launchpad/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/uxstate/launchpad/presentation/ui/theme/Theme.kt
@@ -1,13 +1,16 @@
 package com.uxstate.launchpad.presentation.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorPalette = darkColors(
+private val DarkColorScheme = darkColorScheme(
     primary = Black900,
     secondary = Pink800,
 
@@ -20,7 +23,7 @@ private val DarkColorPalette = darkColors(
 
 )
 
-private val LightColorPalette = lightColors(
+private val LightColorScheme = lightColorScheme(
     primary = Black900,
     secondary = Pink300,
 
@@ -42,17 +45,25 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun LaunchPadTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
+fun LaunchPadTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+        // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
     }
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
+    androidx.compose.material3.MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
     )
 }
