3be197f57bbb71f23ac841f798039a2ecc6a7b82
Tonnie <vontonnie@gmail.com>
2024-08-12 18:30:10 +0300

    Update SimpleAlertDialog.kt

--- app/src/main/java/com/uxstate/launchpad/presentation/common/SimpleAlertDialog.kt
+++ app/src/main/java/com/uxstate/launchpad/presentation/common/SimpleAlertDialog.kt
@@ -4,18 +4,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import com.uxstate.launchpad.R
 import com.uxstate.launchpad.utils.LocalSpacing
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SimpleAlertDialog(
     isShowDialog: Boolean,
@@ -26,36 +29,33 @@ fun SimpleAlertDialog(
     val spacing = LocalSpacing.current
     if (isShowDialog) {
         AlertDialog(
-            onDismissRequest = onDismiss,
-            buttons = {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
-                ) {
+                onDismissRequest = onDismiss,
+                confirmButton = {
+
                     TextButton(onClick = onConfirm) {
                         Text(
-                            text = stringResource(id = R.string.dialog_ok),
-                            color = MaterialTheme.colors.secondary
+                                text = stringResource(id = R.string.dialog_ok),
+                                color = MaterialTheme.colorScheme.secondary
                         )
                     }
+                },
+
+                title = {
+                    Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(text = stringResource(R.string.dialog_error_title))
+                    }
+                },
+                text = {
+                    Text(text = stringResource(id = R.string.dialog_error_text))
                 }
-            },
-            title = {
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = stringResource(R.string.dialog_error_title))
-                }
-            },
-            text = {
-                Text(text = stringResource(id = R.string.dialog_error_text))
-            }
         )
     }
 }
 
-@Preview
+@PreviewLightDark
 @Composable
 fun SimpleAlertDialogPreview() {
 
