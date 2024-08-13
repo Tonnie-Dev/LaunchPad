4d62964d7cd274d1c88d9d7b30a18495ea8aae28
Tonnie <vontonnie@gmail.com>
2024-08-13 20:50:29 +0300

    Update PadCard.kt

--- app/src/main/java/com/uxstate/launchpad/presentation/screens/details/components/PadCard.kt
+++ app/src/main/java/com/uxstate/launchpad/presentation/screens/details/components/PadCard.kt
@@ -1,7 +1,12 @@
 package com.uxstate.launchpad.presentation.screens.details.components
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,60 +28,58 @@ fun PadCard(
 
     val spacing = LocalSpacing.current
     Card(
-        elevation = spacing.spaceExtraSmall,
-        modifier = modifier.padding(spacing.spaceSmall)
+            elevation = CardDefaults.cardElevation(defaultElevation = spacing.spaceExtraSmall),
+            modifier = modifier.padding(spacing.spaceSmall)
     ) {
 
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Text(
-                text = launch.pad.locationName,
-                style = MaterialTheme.typography.h5,
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
+                    text = launch.pad.locationName,
+                    style = MaterialTheme.typography.headlineSmall,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
             )
 
             Text(
-                text = launch.pad.complex,
-                style = MaterialTheme.typography.body1,
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
+                    text = launch.pad.complex,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
             )
 
             Row(horizontalArrangement = Arrangement.SpaceBetween) {
                 PadCounter(
-                    value = launch.pad.totalLaunchCount,
-                    countDesc = stringResource(R.string.total_launches_text)
+                        value = launch.pad.totalLaunchCount,
+                        countDesc = stringResource(R.string.total_launches_text)
                 )
 
                 PadCounter(
-                    value = launch.pad.totalLandingCount,
-                    countDesc = stringResource(R.string.landing_count_text)
+                        value = launch.pad.totalLandingCount,
+                        countDesc = stringResource(R.string.landing_count_text)
                 )
 
                 PadCounter(
-                    value = launch.pad.total,
-                    countDesc = stringResource(R.string.total_text)
+                        value = launch.pad.total,
+                        countDesc = stringResource(R.string.total_text)
                 )
             }
 
             Button(
-                onClick = {
+                    onClick = {
 
-                    val latitude = launch.pad.latitude.toDouble()
-                    val longitude = launch.pad.longitude.toDouble()
+                        val latitude = launch.pad.latitude.toDouble()
+                        val longitude = launch.pad.longitude.toDouble()
 
-                    onClickViewMap(latitude, longitude)
-                },
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor =
-                    MaterialTheme.colors.secondary,
-                    contentColor =
-                    MaterialTheme.colors.onSecondary
-                )
+                        onClickViewMap(latitude, longitude)
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.secondary,
+                            contentColor = MaterialTheme.colorScheme.onSecondary
+                    )
             ) {
 
                 Text(text = stringResource(R.string.view_map_button_text))
@@ -90,20 +93,20 @@ fun PadCounter(value: Int, countDesc: String, modifier: Modifier = Modifier) {
 
     val spacing = LocalSpacing.current
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.padding(spacing.spaceSmall)
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier.padding(spacing.spaceSmall)
     ) {
         Text(
-            text = value.toString(),
-            fontWeight = FontWeight.Bold,
-            style = MaterialTheme.typography.h4,
-            color = MaterialTheme.colors.secondary
+                text = value.toString(),
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.headlineMedium,
+                color = MaterialTheme.colorScheme.secondary
         )
 
         Text(
-            text = countDesc,
-            style = MaterialTheme.typography.caption,
-            fontWeight = FontWeight.Black
+                text = countDesc,
+                style = MaterialTheme.typography.bodySmall,
+                fontWeight = FontWeight.Black
         )
     }
 }
