e645a5ff3e33bfdd1191e706f789cec8b9154eee
Tonnie <vontonnie@gmail.com>
2024-08-13 21:11:13 +0300

    Update FullScreen.kt

--- app/src/main/java/com/uxstate/launchpad/presentation/screens/fullPhoto/FullScreen.kt
+++ app/src/main/java/com/uxstate/launchpad/presentation/screens/fullPhoto/FullScreen.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
