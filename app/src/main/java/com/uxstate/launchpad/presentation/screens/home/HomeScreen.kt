730a0e8b9b3e4f92b2df9a40a2abed02d0a81964
Tonnie <vontonnie@gmail.com>
2024-08-14 08:32:49 +0300

    Update HomeScreen.kt

--- app/src/main/java/com/uxstate/launchpad/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/uxstate/launchpad/presentation/screens/home/HomeScreen.kt
@@ -2,22 +2,19 @@ package com.uxstate.launchpad.presentation.screens.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
 import androidx.paging.compose.collectAsLazyPagingItems
-import com.google.accompanist.pager.rememberPagerState
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.uxstate.launchpad.presentation.screens.home.components.tabs.TabItem
 import com.uxstate.launchpad.presentation.screens.home.components.tabs.Tabs
 import com.uxstate.launchpad.presentation.screens.home.components.tabs.TabsContent
 import com.uxstate.launchpad.presentation.screens.home.components.tabs.TopBar
-import com.uxstate.launchpad.presentation.ui.theme.statusBarColor
 
 
 @Destination()
@@ -26,14 +23,13 @@ fun HomeScreen(viewModel: HomeViewModel = hiltViewModel(), navigator: Destinatio
 
     val previousLaunches = viewModel.previousLaunches.collectAsLazyPagingItems()
     val upcomingLaunches = viewModel.upcomingLaunches.collectAsLazyPagingItems()
-    val pagerState = rememberPagerState()
-    val navController = rememberNavController()
+
     val tabs = listOf(
         TabItem.Upcoming(data = upcomingLaunches, navigator = navigator),
         TabItem.Previous(data = previousLaunches, navigator = navigator)
     )
-    val uiController = rememberSystemUiController()
-    uiController.setStatusBarColor(color = MaterialTheme.colorScheme.statusBarColor)
+
+    val pagerState = rememberPagerState {tabs.size}
 
     Scaffold(topBar = { TopBar() }) { paddingValues ->
         Column(modifier = Modifier.padding(paddingValues)) {
