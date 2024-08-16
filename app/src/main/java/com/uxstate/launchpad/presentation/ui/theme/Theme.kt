966dcf70b8d441aa9d2623fef9193bd1ab5e2381
Tonnie <vontonnie@gmail.com>
2024-08-16 07:35:57 +0300

    Update Theme to Disable Dynamic Colors

--- app/src/main/java/com/uxstate/launchpad/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/uxstate/launchpad/presentation/ui/theme/Theme.kt
@@ -266,22 +266,22 @@ fun LaunchPadTheme(
     content: @Composable() () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+       /* dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+        }*/
 
         darkTheme -> darkScheme
         else -> lightScheme
     }
     val view = LocalView.current
-    if (!view.isInEditMode) {
+   /* if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
-    }
+    }*/
 
     MaterialTheme(
             colorScheme = colorScheme,
