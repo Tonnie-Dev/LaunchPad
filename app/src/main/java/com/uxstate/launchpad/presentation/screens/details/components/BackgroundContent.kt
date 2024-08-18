3eb3e656f84e6c24d0e6f24503f235e46cc60f5f
Tonnie <vontonnie@gmail.com>
2024-08-18 13:41:49 +0300

    Update BackgroundContent.kt

--- app/src/main/java/com/uxstate/launchpad/presentation/screens/details/components/BackgroundContent.kt
+++ app/src/main/java/com/uxstate/launchpad/presentation/screens/details/components/BackgroundContent.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
@@ -60,6 +61,7 @@ fun BackgroundContent(
 
 }
 
+@Preview(showSystemUi = true)
 @PreviewLightDark
 @Composable
 private fun BackgroundContentPreview() {
