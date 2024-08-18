05e1822c6c316e1365c99abf54116936687aed9c
Tonnie <vontonnie@gmail.com>
2024-08-18 13:42:03 +0300

    Update LaunchBottomSheet.kt

--- app/src/main/java/com/uxstate/launchpad/presentation/screens/details/components/LaunchBottomSheet.kt
+++ app/src/main/java/com/uxstate/launchpad/presentation/screens/details/components/LaunchBottomSheet.kt
@@ -1,5 +1,6 @@
 package com.uxstate.launchpad.presentation.screens.details.components
 
+import android.content.Context
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -8,11 +9,44 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import com.uxstate.launchpad.domain.model.Launch
+import com.uxstate.launchpad.presentation.screens.common.SimpleAlertDialog
 import com.uxstate.launchpad.presentation.ui.theme.LaunchPadTheme
 import com.uxstate.launchpad.utils.generateLaunch
+import com.uxstate.launchpad.utils.openGoogleMap
+
 
 @Composable
 fun LaunchBottomSheet(
+    launch: Launch,
+    isShowDialog: Boolean,
+    context: Context,
+    onDismissDialog: () -> Unit,
+    onConfirmDialog: () -> Unit,
+    onShowDialog: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    SimpleAlertDialog(
+            isShowDialog = isShowDialog,
+            onDismiss = onDismissDialog,
+            onConfirm = onConfirmDialog
+    )
+    LaunchBottomSheetContent(
+            modifier = modifier,
+            launch = launch,
+            onClickViewMap = { latitude, longitude ->
+
+                if (latitude == 0.0 || longitude == 0.0) {
+
+                    onShowDialog()
+                } else {
+                    openGoogleMap(latitude, longitude, context)
+                }
+            }
+    )
+}
+
+@Composable
+fun LaunchBottomSheetContent(
     launch: Launch,
     onClickViewMap: (Double, Double) -> Unit,
     modifier: Modifier = Modifier,
@@ -36,7 +70,7 @@ private fun LaunchBottomSheetContentPreview() {
     LaunchPadTheme {
         Surface {
 
-            LaunchBottomSheet(
+            LaunchBottomSheetContent(
                     launch = generateLaunch(),
                     onClickViewMap = { _, _ -> }
             )
