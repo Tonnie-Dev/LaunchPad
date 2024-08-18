602655a1fc35ad981ceb19fc7d3ad2ccf80c5963
Tonnie <vontonnie@gmail.com>
2024-08-18 18:58:13 +0300

    Upgrade from version 1.0.0 to app version 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.uxstate.launchpad"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
