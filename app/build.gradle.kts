75751db9aeae006b0679bc4c375ca808f4e8b047
Tonnie <vontonnie@gmail.com>
2025-08-26 20:30:22 +0300

    Bump compileSdk 34 -> 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,14 +12,14 @@ plugins {
 
 android {
     namespace = "com.uxstate.launchpad"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.uxstate.launchpad"
         minSdk = 26
-        targetSdk = 34
-        versionCode = 2
-        versionName = "2.0.0"
+        targetSdk = 36
+        versionCode = 3
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
